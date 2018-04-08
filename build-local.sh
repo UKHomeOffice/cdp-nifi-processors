1518f68e22d706a39cb3f20b08c31beb74197643
Pontus <contrib@pontusnetworks.com>
2018-04-08 19:29:08 +0100

    re-activated git

--- build-local.sh
+++ build-local.sh
@@ -9,7 +9,7 @@ fi
 CURDIR=`pwd`
 cd $DIR
 
-#git pull; 
+git pull; 
 mvn -Dmaven.test.skip=true -DskipTests=true -T 8 clean install ; 
 #scp -P 12222 */*/target/*.nar nifi-pontus-elastic-2.x-processor-bundle/nifi-pontus-elastic-2.x-processor/target/nifi-pontus-elastic-2.x-processor-1.0.jar root@localhost:/opt/pontus;
 #scp -P 12222 */*/target/*gremlin*.nar */*/target/*service*.nar root@localhost:/opt/pontus;
