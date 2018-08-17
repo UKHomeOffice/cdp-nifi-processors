ba5ac3268e9eefc2cc651f6dc104da7d795b257d
root <root@client-server0.dev.cdp.internal>
2018-08-17 11:57:56 +0000

    updated branch

--- build-local.sh
+++ build-local.sh
@@ -10,11 +10,11 @@ CURDIR=`pwd`
 cd $DIR
 
 git pull; 
-mvn -Dmaven.test.skip=true -DskipTests=true -T 8 clean install ; 
+mvn -Dmaven.test.skip=true -DskipTests=true  clean install ; 
 #scp -P 12222 */*/target/*.nar nifi-pontus-elastic-2.x-processor-bundle/nifi-pontus-elastic-2.x-processor/target/nifi-pontus-elastic-2.x-processor-1.0.jar root@localhost:/opt/pontus;
 #scp -P 12222 */*/target/*gremlin*.nar */*/target/*service*.nar root@localhost:/opt/pontus;
 #cp  */*/target/*gremlin*.nar */*/target/*service*.nar ../nifi-1.2.0.3.0.1.1-5/lib
-cp  */*/target/*gremlin*.nar */*/target/*service*.nar $OUT_DIR
+cp */*/target/*office*.nar  */*/target/*gremlin*.nar */*/target/*service*.nar $OUT_DIR
 
 cd $CURDIR
 
